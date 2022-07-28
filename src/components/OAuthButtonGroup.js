3e343a215019f052edd761ecf4536da9dea1ccfa
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-07-28 15:51:50 +0530

    Updated Login Page

--- src/components/OAuthButtonGroup.js
+++ src/components/OAuthButtonGroup.js
@@ -1,5 +1,6 @@
 import { Button, ButtonGroup, VisuallyHidden } from '@chakra-ui/react'
 import { GitHubIcon, GoogleIcon, TwitterIcon } from './ProviderIcons'
+import React from 'react'
 const providers = [
   {
     name: 'Google',
@@ -15,13 +16,40 @@ const providers = [
   },
 ]
 
-export const OAuthButtonGroup = () => (
-  <ButtonGroup variant="outline" spacing="4" width="full">
-    {providers.map(({ name, icon }) => (
-      <Button key={name} width="full">
-        <VisuallyHidden>Sign in with {name}</VisuallyHidden>
-        {icon}
-      </Button>
-    ))}
-  </ButtonGroup>
-)
+export const OAuthButtonGroup = React.forwardRef((props, ref) => {
+  return (
+    <ButtonGroup variant="outline" spacing="4" width="full">
+      {providers.map(({ name, icon }) => (
+        <Button key={name} width="full" {...props}>
+          <VisuallyHidden>Sign in with {name}</VisuallyHidden>
+          {icon}
+        </Button>
+      ))}
+    </ButtonGroup>
+  )
+})
+
+
+// export const OAuthButton = React.forwardRef((props, ref) => {
+//   const providers = [
+//     {
+//       name: 'Google',
+//       icon: <GoogleIcon boxSize="5" />,
+//     },
+//     {
+//       name: 'Twitter',
+//       icon: <TwitterIcon boxSize="5" />,
+//     },
+//     {
+//       name: 'GitHub',
+//       icon: <GitHubIcon boxSize="5" />,
+//     },
+//   ]
+  
+//   return (
+//     <Button key={props.name} width="full" {...props}>
+//       <VisuallyHidden>Sign in with {props.name}</VisuallyHidden>
+//       {props.icon}
+//     </Button>
+//   )
+// })
--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -158,7 +158,6 @@ export default function Loginpage() {
                   </FormControl>
                   <PasswordField
                     name='password'
-                    type='password'
                     autoComplete='password'
                     value={password}
                     required
