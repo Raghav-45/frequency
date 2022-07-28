10a27c77fd0f69b95eebd5ab1f2507000adb7a4b
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2022-07-28 15:48:42 +0530

    Update Loginpage.jsx

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {
+  chakra,
   Box,
   Button,
   Checkbox,
@@ -11,32 +12,48 @@ import {
   HStack,
   Input,
   Stack,
+  useToast,
   Text,
   useBreakpointValue,
   useColorModeValue,
+  ButtonGroup,
 } from '@chakra-ui/react'
+import { VisuallyHidden } from '@chakra-ui/react'
+import { GitHubIcon, GoogleIcon, TwitterIcon } from '../components/ProviderIcons'
 import { Layout } from '../components/Layout'
 import { Link, useHistory, useLocation } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import useMounted from '../hooks/useMounted'
-import { OAuthButtonGroup } from '../components/OAuthButtonGroup'
+// import { OAuthButton } from '../components/OAuthButtonGroup'
 import { PasswordField } from '../components/PasswordField'
 
 export default function Loginpage() {
+  const { signInWithGoogle, login } = useAuth()
   const history = useHistory()
   const location = useLocation()
   const mounted = useMounted()
+  const toast = useToast()
+  const [isSubmitting, setIsSubmitting] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const iconProviders = [
+    {
+      name: 'Google',
+      icon: <GoogleIcon boxSize="5" />,
+    },
+    {
+      name: 'Twitter',
+      icon: <TwitterIcon boxSize="5" />,
+    },
+    {
+      name: 'GitHub',
+      icon: <GitHubIcon boxSize="5" />,
+    },
+  ]
+
   function handleRedirectToOrBack() {
-    // console.log(location?.state)
     history.replace(location.state?.from ?? '/profile')
-    // if (location.state) {
-    //   history.replace(location.state?.from)
-    // } else {
-    //   history.replace('/profile')
-    // }
   }
   
   return (
@@ -62,7 +79,7 @@ export default function Loginpage() {
               </Heading>
               <HStack spacing="1" justify="center">
                 <Text color="muted">Don't have an account?</Text>
-                <Button variant="link" colorScheme="blue">
+                <Button variant="link" colorScheme="blue" onClick={() => history.push('/register')}>
                   Sign up
                 </Button>
               </HStack>
@@ -90,46 +107,117 @@ export default function Loginpage() {
               sm: 'xl',
             }}
           >
-            <Stack spacing="6">
-              <Stack spacing="5">
-                <FormControl>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <Input
-                    id="email"
-                    type='email'
-                    autoComplete='email'
-                    required
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                  />
-                </FormControl>
-                <PasswordField
-                  name='password'
-                  type='password'
-                  autoComplete='password'
-                  value={password}
-                  required
-                  onChange={e => setPassword(e.target.value)}
-                />
-              </Stack>
-              <HStack justify="space-between">
-                <Checkbox defaultChecked>Remember me</Checkbox>
-                <Button variant="link" colorScheme="blue" size="sm">
-                  Forgot password?
-                </Button>
-              </HStack>
+            <chakra.form
+              onSubmit={async e => {
+                e.preventDefault()
+                if (!email || !password) {
+                  toast({
+                    description: 'Credentials not valid.',
+                    status: 'error',
+                    duration: 9000,
+                    isClosable: true,
+                  })
+                  return
+                }
+                // your login logic here
+                setIsSubmitting(true)
+                login(email, password)
+                  .then(res => {
+                    handleRedirectToOrBack()
+                  })
+                  .catch(error => {
+                    console.log(error.message)
+                    toast({
+                      description: error.message,
+                      status: 'error',
+                      duration: 9000,
+                      isClosable: true,
+                    })
+                  })
+                  .finally(() => {
+                    // setTimeout(() => {
+                    //   mounted.current && setIsSubmitting(false)
+                    //   console.log(mounted.current)
+                    // }, 1000)
+                    mounted.current && setIsSubmitting(false)
+                  })
+              }}
+              >
               <Stack spacing="6">
-                <Button variant="primary">Sign in</Button>
-                <HStack>
-                  <Divider />
-                  <Text fontSize="sm" whiteSpace="nowrap" color="muted">
-                    or continue with
-                  </Text>
-                  <Divider />
+                <Stack spacing="5">
+                  <FormControl>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      id="email"
+                      type='email'
+                      autoComplete='email'
+                      required
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
+                    />
+                  </FormControl>
+                  <PasswordField
+                    name='password'
+                    type='password'
+                    autoComplete='password'
+                    value={password}
+                    required
+                    onChange={e => setPassword(e.target.value)}
+                  />
+                </Stack>
+                <HStack justify="space-between">
+                  <Checkbox defaultChecked>Remember me</Checkbox>
+                  <Button variant="link" colorScheme="blue" size="sm">
+                    Forgot password?
+                  </Button>
                 </HStack>
-                <OAuthButtonGroup />
+                <Stack spacing="6">
+                  <Button
+                    type='submit'
+                    colorScheme='blue'
+                    size='lg'
+                    fontSize='md'
+                    isLoading={isSubmitting}
+                  >
+                    Sign in
+                  </Button>
+                  <HStack>
+                    <Divider />
+                    <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                      or continue with
+                    </Text>
+                    <Divider />
+                  </HStack>
+                  <ButtonGroup variant="outline" spacing="4" width="full">
+                  <Button key='Google' width="full"
+                    onClick={() =>
+                      signInWithGoogle()
+                        .then(user => {
+                          handleRedirectToOrBack()
+                          console.log(user)
+                        })
+                        .catch(e => console.log(e.message))
+                    }
+                  >
+                    <VisuallyHidden>Sign in with 'Google'</VisuallyHidden>
+                    {iconProviders[0].icon}
+                  </Button>
+                  <Button key='Twitter' width="full"
+
+                  >
+                    <VisuallyHidden>Sign in with 'Twitter'</VisuallyHidden>
+                    {iconProviders[1].icon}
+                  </Button>
+                  <Button key='Github' width="full"
+
+                  >
+                    <VisuallyHidden>Sign in with 'Github'</VisuallyHidden>
+                    {iconProviders[2].icon}
+                  </Button>
+                  </ButtonGroup>
+                </Stack>
               </Stack>
-            </Stack>
+            </chakra.form>
           </Box>
         </Stack>
       </Container>
